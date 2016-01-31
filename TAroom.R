8eddb7f50b403a2c2af91533cfd71775ffbe123c
lcolladotor <lcollado@jhsph.edu>
2016-01-31 11:46:36 -0500

    Update room info

--- TAroom.R
+++ TAroom.R
@@ -1,13 +1,13 @@
 ## Extract rooms
 rooms <- read.csv("MPH TA office hours.csv", stringsAsFactors = FALSE, na.strings = "")
 rooms <- rooms[complete.cases(rooms), ]
-rooms$date <- as.Date(rooms$Start.Date, format = "%m/%d/%y")
+rooms$date <- as.Date(rooms$Date, format = "%m/%d/%y")
 rooms <- rooms[order(rooms$date), ]
 rooms$dateChar <- as.character(rooms$date)
 
-tas <- c("Leo", "Youssef", "Danielle", "Molly")
+tas <- c("Leo", "Youssef", "Danielle", "Emily")
 TAroom <- lapply(tas, function(ta) {
-    use <- rooms[grepl(ta, rooms$Name), ]
+    use <- rooms[grepl(ta, rooms$TA), ]
     res <- use$Room
     names(res) <- use$dateChar
     return(res)
